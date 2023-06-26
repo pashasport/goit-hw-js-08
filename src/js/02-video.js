dee0f06a86a70d0ab76fc59df33f6bf5398f4e21
pashasport <123305807+pashasport@users.noreply.github.com>
2023-06-26 23:45:33 +0300

    hw-for-check-change-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,3 @@
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
-// const iframe = document.querySelector('iframe');
-// const CURRENT_TIME_KEY = 'videoplayer-current-time';
-// const player = new Player(iframe);
-// const getCurrentTime = function (currentTime) {
-//     localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(currentTime.seconds));
-//  };
-
-// player.on('timeupdate', throttle(getCurrentTime, 1000));
-// const saveCurrentTime(JSON.parse(saveCurrentTime) || 0);
-
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
